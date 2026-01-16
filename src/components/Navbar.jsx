3d39b6c623fc20a9f2a7154e3a0460b685f38863
the-abed <mohammadabed1283@gmail.com>
2026-01-16 23:07:31 +0600

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div className='px-5 py-4 flex justify-between gap-5 bg-stone-800'>
-            <h2 className='text-white'>Restaurant</h2>
-            <ul className='flex gap-5'>
-                <li className='text-white'>Home</li>
-                <li className='text-white'>Menu</li>
-                <li className='text-white'>About</li>
-                <li className='text-white'>Contact</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="px-5 py-4 flex justify-between gap-5 bg-stone-800">
+      <h2 className="font-bold  text-blue-500">Restaurant</h2>
+      <ul className="flex gap-5">
+        <Link href="/foods">
+          <li>Foods</li>
+        </Link>
+        <Link href="/reviews">
+          <li>Reviews</li>
+        </Link>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
