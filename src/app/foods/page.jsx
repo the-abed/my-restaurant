26a0af56bbcaa8c2e73785c07a1ddb202495b69b
the-abed <mohammadabed1283@gmail.com>
2026-01-23 23:50:11 +0600

    add revalidation in food page

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -4,7 +4,13 @@ import CartItems from './CartItems';
 import InputSearch from '@/components/InputSearch';
 
 const getFoods = async (search) => {
-    const res = await fetch(`https://taxi-kitchen-api.vercel.app/api/v1/foods/random?search=${search}`);
+    const res = await fetch(`https://taxi-kitchen-api.vercel.app/api/v1/foods/random?search=${search}`,
+    {
+        next: {
+            revalidate: 10
+        }
+    }
+    );
     
     const data = await res.json();
     await new Promise((resolve) => setTimeout(resolve, 2000));
