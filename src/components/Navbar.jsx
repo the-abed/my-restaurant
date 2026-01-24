3366c5c42dbde9194bfb8d530fb5c61fdb4bc10a
the-abed <mohammadabed1283@gmail.com>
2026-01-24 23:19:30 +0600

    add favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import logo from "../../public/logo.png";
 
 const Navbar = () => {
   return (
     <div className="px-5 py-4 flex justify-between gap-5 bg-stone-900">
-      <Link href="/"><span className="text-2xl font-bold text-yellow-500">🍟My Restarant🧋</span></Link>
+      <Link href="/"><span className="text-2xl font-bold text-yellow-500">🍟My Restaurant🧋</span></Link>
       <ul className="flex gap-4">
         <Link href="/foods">
           <li className="btn font-semibold">Foods</li>
